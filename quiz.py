69cfe534a7627f34f6c2e9e934f5455af16440ed
hanganaganuma <130372287+hanganaganuma@users.noreply.github.com>
2023-10-24 15:42:33 +0900

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,12 @@
+print('バント名クリエイターへようこそ')
+area = input('出身地はどこですか')
+charactor = input('好きなキャラクターはなんですか？')
+print(area + charactor)
+
+print('割り算計算機へようこそ')
+total = input('お会計はいくらでしたか？')
+chip = input('チップは何％にしますか？')
+people = input('何人で割りますか')
+
+print(total+(1*(chip/100))/people)
+
